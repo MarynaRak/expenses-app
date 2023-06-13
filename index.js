6bf02017e975ca5b40dab5fae95245411144f58a
MarynaRak <0633599104s@gmail.com>
2023-06-14 00:16:08 +0300

    limit commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const LIMIT = 0;
+let LIMIT;
 const CURRENCY = 'грн.';
 const STATUS_IN_LIMIT = 'Все хорошо';
-const STATUS_OUT_OF_LIMIT = 'Все Плохо';
+const STATUS_OUT_OF_LIMIT = 'Все плохо';
 const STATUS_OUT_OF_LIMIT_CLASSNAME = 'limit_red';
 
 const inputNode = document.querySelector('.js-input');
@@ -17,11 +17,16 @@ const BODY_FIXED_CLASSNAME = "body-fixed";
 
 const bodyNode = document.querySelector('body');
 const popupNode = document.querySelector('.js-popup');
-const popupContentNode = document.querySelector('.js-popup-close-btn');
-const popupOpenBtn = document.querySelector('.js-edit-btn');
-const popupClosedBtn = document.querySelector('.js-popup-close-btn');
+const popupContentNode = document.querySelector('.js-popup-content');
+const popupOpenBtnNode = document.querySelector('.js-edit-btn');
+// const popupClosedBtnNode = document.querySelector('.js-popup-close-btn');
 
 const limitInputNode = document.querySelector('.js-add-limit-input');
+const popupAddLimitBtn = document.querySelector('.js-popup-add-limit-btn');
+const emptyLimitOutputMessage = document.querySelector('.empty-limit')
+
+popupOpenBtnNode.addEventListener('click', togglePopup);
+// popupClosedBtnNode.addEventListener('click', togglePopup);
 
 const expenses = [];
  
@@ -41,12 +46,11 @@ buttonNode.addEventListener('click', function () {
    });
 
 function init(expenses) {
-    limitNode.innerText = LIMIT;
     statusNode.innerText = STATUS_IN_LIMIT;
     sumNode.innerText = calculateExpences(expenses);
 }
 
-function calculateExpences(expenses) {
+function calculateExpences() {
     let sum = 0;
     expenses.forEach(element => {
         sum += element;
@@ -80,11 +84,12 @@ function renderSum(expenses) {
 
 function renderStatus(expenses) {
     const sum = calculateExpences(expenses);
+    const outOfSum = sum - LIMIT;
 
     if (sum <= LIMIT) {
         statusNode.innerText = STATUS_IN_LIMIT;
     } else {
-        statusNode.innerText = STATUS_OUT_OF_LIMIT;
+        statusNode.innerText = `${STATUS_OUT_OF_LIMIT} (- ${outOfSum} ${CURRENCY})`;
         statusNode.classList.add(STATUS_OUT_OF_LIMIT_CLASSNAME);
     };
 }
@@ -105,9 +110,39 @@ function getExpencesFromUser() {
 
 function clearInput() {
     inputNode.value = '';
-
+    limitInputNode.value = '';
 }
 
 // let selectedIndex = selectedValue.selectedIndex;
 // let options = selectedValue.options;
 // let content = options[selectedIndex].textContent;
+
+
+popupNode.addEventListener('click', (event) => {
+    const clickOutsideContent = !event.composedPath().includes(popupContentNode)
+
+    if (clickOutsideContent) {
+        togglePopup();
+    }
+})
+
+function togglePopup() {
+    popupNode.classList.toggle(POPUP_OPENED_CLASSNAME);
+    bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+}
+
+popupAddLimitBtn.addEventListener('click', getLimitFromUser);
+
+function getLimitFromUser() {
+    LIMIT = parseInt(limitInputNode.value);
+
+    if (!limitInputNode.value) {
+        emptyLimitOutputMessage.innerText = 'Необходимо ввести значение';
+        return;
+    } 
+
+    clearInput();
+    togglePopup();
+    return limitNode.innerText = LIMIT;
+}
+
